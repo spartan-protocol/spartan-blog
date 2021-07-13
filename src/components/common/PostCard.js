cbff001ead35436cfb72602007cb29f559cece72
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-07-14 00:13:07 +0800

    styling init
    - general header structure complete
    - general post array structure complete

--- src/components/common/PostCard.js
+++ src/components/common/PostCard.js
@@ -1,42 +1,50 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link } from 'gatsby'
-import { Tags } from '@tryghost/helpers-gatsby'
-import { readingTime as readingTimeHelper } from '@tryghost/helpers'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
+import { Tags } from "@tryghost/helpers-gatsby";
+import { readingTime as readingTimeHelper } from "@tryghost/helpers";
 
 const PostCard = ({ post }) => {
-    const url = `/${post.slug}/`
-    const readingTime = readingTimeHelper(post)
+    const url = `/${post.slug}/`;
+    const readingTime = readingTimeHelper(post);
+    const postedDate = new Date(post.published_at);
 
     return (
         <Link to={url} className="post-card">
             <header className="post-card-header">
-                {post.feature_image &&
-                    <div className="post-card-image" style={{
-                        backgroundImage: `url(${post.feature_image})` ,
-                    }}></div>}
-                {post.tags && <div className="post-card-tags"> <Tags post={post} visibility="public" autolink={false} /></div>}
-                {post.featured && <span>Featured</span>}
-                <h2 className="post-card-title">{post.title}</h2>
+                {post.feature_image && (
+                    <div
+                        className="post-card-image"
+                        style={{
+                            backgroundImage: `url(${post.feature_image})`,
+                        }}
+                    ></div>
+                )}
             </header>
-            <section className="post-card-excerpt">{post.excerpt}</section>
+            <section className="post-card-excerpt">
+                {postedDate.toLocaleDateString("en-US")}
+                <h2 className="post-card-title">{post.title}</h2>
+                {post.excerpt}
+            </section>
             <footer className="post-card-footer">
                 <div className="post-card-footer-left">
-                    <div className="post-card-avatar">
-                        {post.primary_author.profile_image ?
-                            <img className="author-profile-image" src={post.primary_author.profile_image} alt={post.primary_author.name}/> :
-                            <img className="default-avatar" src="/images/icons/avatar.svg" alt={post.primary_author.name}/>
-                        }
-                    </div>
-                    <span>{ post.primary_author.name }</span>
+                    {post.tags && (
+                        <div className="post-card-tags">
+                            <Tags
+                                post={post}
+                                visibility="public"
+                                autolink={false}
+                            />
+                        </div>
+                    )}
                 </div>
                 <div className="post-card-footer-right">
                     <div>{readingTime}</div>
                 </div>
             </footer>
         </Link>
-    )
-}
+    );
+};
 
 PostCard.propTypes = {
     post: PropTypes.shape({
@@ -44,6 +52,7 @@ PostCard.propTypes = {
         title: PropTypes.string.isRequired,
         feature_image: PropTypes.string,
         featured: PropTypes.bool,
+        published_at: PropTypes.string,
         tags: PropTypes.arrayOf(
             PropTypes.shape({
                 name: PropTypes.string,
@@ -55,6 +64,6 @@ PostCard.propTypes = {
             profile_image: PropTypes.string,
         }).isRequired,
     }).isRequired,
-}
+};
 
-export default PostCard
+export default PostCard;
