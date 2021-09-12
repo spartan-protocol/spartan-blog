b0e3f289a8c93693b614c91191af37d9dc6390e1
Samus <26967164+SamusElderg@users.noreply.github.com>
2021-09-12 10:12:49 +0800

    Update index meta

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -4,12 +4,12 @@ module.exports = {
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     siteTitleMeta: `Spartan Protocol Blog`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Updated news and announcements from the Spartan Protocol community`, // This allows an alternative site description for meta data for pages.
+    siteDescriptionMeta: `Your official source for news & announcements from the community built & run Spartan Protocol project. Keep up to date with all the latest dev updates, how-to guides & general information.`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Spartan Blog`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `SpartanProtocol Blog`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
